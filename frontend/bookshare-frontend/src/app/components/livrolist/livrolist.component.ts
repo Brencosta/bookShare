a00ac2a103d39dde11462d9e008d9619545c3ccc
breno <brenocunha977@gmail.com>
2026-04-21 10:42:16 -0300

    criando componente lista e dashboard

---
+++ frontend/bookshare-frontend/src/app/components/livrolist/livrolist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livrolist',
+  imports: [],
+  templateUrl: './livrolist.component.html',
+  styleUrl: './livrolist.component.css'
+})
+export class LivrolistComponent {
+
+}
